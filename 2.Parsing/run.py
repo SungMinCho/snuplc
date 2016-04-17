c18b2c2048eb84bdabdda9a7386044737420f223
SungMinCho <tjdals4565@gmail.com>
2016-04-17 19:53:01 +0900

    Add run.py

---
+++ 2.Parsing/run.py
@@ -0,0 +1,33 @@
+import subprocess
+import glob
+import difflib
+
+def main():
+
+  for f in glob.glob("./test/**/*.mod"):
+    with open("ref", "w") as refout, open("you", "w") as youout:
+      print("running", f)
+      print("diff reference and yours")
+
+      ref = subprocess.Popen(["./reference/test_parser", f], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+      refout.write(ref.stdout.read().decode())
+      refout.write(ref.stderr.read().decode())
+
+      yours = subprocess.Popen(["./snuplc/test_parser", f], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+      youout.write(yours.stdout.read().decode())
+      youout.write(yours.stderr.read().decode())
+
+    with open("ref", "r") as refin, open("you", "r") as youin:
+      reflines = refin.readlines()
+      youlines = youin.readlines()
+
+      d = difflib.Differ()
+      diff = d.compare(reflines, youlines)
+      print("".join(diff))
+
+    subprocess.Popen(["rm", "ref"]).communicate()
+    subprocess.Popen(["rm", "you"]).communicate()
+
+
+if __name__ == "__main__":
+  main()
