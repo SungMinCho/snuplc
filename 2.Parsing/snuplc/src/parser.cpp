0b163438d652010ad1d9990bb6b182b4754dc873
SungMinCho <tjdals4565@gmail.com>
2016-04-18 09:35:43 +0900

    Do character in factor

--- 2.Parsing/snuplc/src/parser.cpp
+++ 2.Parsing/snuplc/src/parser.cpp
@@ -549,6 +549,8 @@ CAstExpression* CParser::factor(CAstScope *s)
     // TODO
   } else if(tt == tCharacter) {
     // TODO
+    Consume(tCharacter, &t);
+    n = new CAstConstant(t, CTypeManager::Get()->GetChar(), (int)t.GetValue()[0]);
   } else if(tt == tString) {
     // TODO
   } else if(tt == tLBrak) {
