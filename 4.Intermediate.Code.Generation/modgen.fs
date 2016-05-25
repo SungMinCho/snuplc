0dda39de89ff295aebca5fc70691a45dbf12765d
SungMinCho <tjdals4565@gmail.com>
2016-05-25 17:18:47 +0900

    Save fs

--- 4.Intermediate.Code.Generation/modgen.fs
+++ 4.Intermediate.Code.Generation/modgen.fs
@@ -19,11 +19,14 @@ and factOp = Mult | Div | And
 and posneg = Pos | Neg
 and bang = Bang
 and call = Call0 of name * expr list
+and basetyp = NULL | INT | CHAR | BOOL
+and typ = Typ0 of basetyp | Typ1 of typ * int
 
-type symtab = string (*TODO*)
+type symtab = Dictionary<typ, name>
 
-let get_int (st:symtab) : qualident = Qualident0 "TODO"
-let get_bool (st:symtab) : qualident = Qualident0 "TODO"
+let get_int (depth:int) (st:symtab) : qualident = Qualident0 "TODO"
+let get_bool (depth:int) (st:symtab) : qualident = Qualident0 "TODO"
+let get_char (depth:int) (st:symtab) : qualident = Qualident0 "TODO"
 
 let rec make_int_expr (depth:int) (st:symtab) : expr = Expr0 (make_int_simplexpr (depth-1) st)
 and make_int_simplexpr (depth:int) (st:symtab) : simplexpr = 
@@ -40,7 +43,7 @@ and make_int_term (depth:int) (st:symtab) : term =
          Term1 ((make_int_term (depth-1) st), op, (make_int_factor (depth-1) st))
 and make_int_factor (depth:int) (st:symtab) : factor =
   match (getRandom 4) with
-  | 0 -> Factor0 (get_int st)
+  | 0 -> Factor0 (get_int (depth-1) st)
   | 1 -> Factor1 (getRandom 1000000)
   | 2 -> Factor5 (make_int_expr (depth-1) st)
   | _ -> Factor6 (make_int_call (depth-1) st)
@@ -66,7 +69,7 @@ and make_bool_term (depth:int) (st:symtab) : term =
   | _ -> Term1 ((make_bool_term (depth-1) st), And, (make_bool_factor (depth-1) st))
 and make_bool_factor (depth:int) (st:symtab) : factor =
   match (getRandom 5) with
-  | 0 -> Factor0 (get_bool st)
+  | 0 -> Factor0 (get_bool (depth-1) st)
   | 1 -> let v = (match (getRandom 2) with 0 -> true | _ -> false) in
          Factor2 v
   | 2 -> Factor5 (make_bool_expr (depth-1) st)
@@ -85,3 +88,43 @@ and make_char_factor (depth:int) (st:symtab) : factor =
   | _ -> Factor6 (make_char_call (depth-1) st)
 and make_char_call (depth:int) (st:symtab) : call =
   Call0 ("TODO", [])
+
+type stat = Stat0 of qualident * expr
+          | Stat1 of call
+          | Stat2 of expr * stat list * stat list
+          | Stat3 of expr * stat list
+          | Stat4 of expr option
+
+exception Return_type_error
+
+let rec make_stat (depth:int) (st:symtab) (rettyp : typ) : stat =
+  match (getRandom 3) with
+  | 0 -> Stat0 ((get_int (depth-1) st), (make_int_expr (depth-1) st))
+  | 1 -> Stat0 ((get_bool (depth-1) st), (make_bool_expr (depth-1) st))
+  | 2 -> Stat0 ((get_char (depth-1) st), (make_char_expr (depth-1) st))
+  | 3 -> Stat1 (make_int_call (depth-1) st)
+  | 4 -> Stat1 (make_bool_call (depth-1) st)
+  | 5 -> Stat1 (make_char_call (depth-1) st) 
+  | 6 -> Stat2 ((make_bool_expr (depth-1) st),
+                [for x in 0 .. (getRandom depth) -> (make_stat (depth-1) st rettyp)],
+                [for x in 0 .. (getRandom depth) -> (make_stat (depth-1) st rettyp)])
+  | 7 -> Stat3 ((make_bool_expr (depth-1) st), [for x in 0 .. (getRandom depth) -> (make_stat (depth-1) st rettyp)])
+  | _ -> match rettyp with
+         | Typ0 NULL -> Stat4 None
+         | Typ0 INT ->  Stat4 (Some (make_int_expr (depth-1) st))
+         | Typ0 CHAR ->  Stat4 (Some (make_char_expr (depth-1) st))
+         | Typ0 BOOL ->  Stat4 (Some (make_bool_expr (depth-1) st))
+         | _ -> raise Return_type_error
+
+
+type func = Func of typ * symtab * stat list
+type md = Md of symtab * func list * stat list
+
+let make_func (depth:int) (sn:int) : func =
+  let t = (match (getRandom 4) with 0 -> Typ0 NULL | 1 -> Typ0 INT | 2 -> Typ0 CHAR | _ -> Typ0 BOOL) in
+  let st = new Dictionary<typ, name>() in
+  Func (t, st, [for x in 1 .. sn -> make_stat depth st t])
+
+let make_module (depth:int) (fn:int) (sn:int) : md =
+  let st = new Dictionary<typ, name>() in
+  Md (st, [for x in 1 .. fn -> make_func depth sn], [for x in 1 .. sn -> make_stat depth st (Typ0 NULL)])
